7f377315f6ffb88cf03bd3d4a4fd71b813ea9ca3
shijo shaji <30753003+shijoshaji95@users.noreply.github.com>
2018-11-08 17:36:08 +0530

    Delete enhance_image.m

--- enhance_image.m
+++
@@ -1,9 +0,0 @@
-[filename, pathname] = uigetfile({'*.*';'*.bmp';'*.jpg';'*.gif'}, 'Pick a Leaf Image File');
-I = imread([pathname,filename]);
-I3 = handles.ImgData1;
-I4 = imadjust(I3,stretchlim(I3));
-I5 = imresize(I4,[300,400]);
-axes(handles.axes2);
-imshow(I5);title(' Contrast Enhanced ');
-handles.ImgData2 = I4;
-guidata(hObject,handles);
