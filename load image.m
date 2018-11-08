2245212185752ebfc32de97be6f02d497f3210da
shijo shaji <30753003+shijoshaji95@users.noreply.github.com>
2018-11-08 17:35:50 +0530

    Delete load image.m

--- load image.m
+++
@@ -1,4 +0,0 @@
-[filename, pathname] = uigetfile({'*.*';'*.bmp';'*.jpg';'*.gif'}, 'Pick a Leaf Image File');
-I = imread([pathname,filename]);
-I = imresize(I,[256,256]);
- figure, imshow(I);title('Loaded Image ');
